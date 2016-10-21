7545d142cd4c9bc17758b66a93b376d3ec916135
Austin Blain <austin.a.blain@gmail.com>
2016-10-21 09:46:33 -0700

    completely change JS from hiding and showing divs to inserting suggestion into span, will also change business end

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,28 @@
-var decidingForYou = function(type, creative, prefer, whatDoYouThink, flavor) {
-  if (type === "Type B" && creative === "Totally" && prefer === "Art" && whatDoYouThink === "Front End for sure" && flavor === "chocolate") {
-    return results1;
-  } else if (type === "Type A" && creative === "Totally" && prefer === "Numbers" && whatDoYouThink === "Front End for sure" && flavor === "chocolate") {
-    return results4;
-  } else if (type === "Type A" && creative === "Totally" && prefer === "Numbers" && whatDoYouThink === "Back End is where the real challange is at" && flavor === "vanilla") {
-    return  results3;
-  } else if (type === "Type B" && creative === "Sort of" && prefer === "Numbers" && whatDoYouThink === "Back End is where the real challange is it" && flavor === "chocolate") {
-    return results2;
+var deciding = function(type, creative, prefer, whatDoYouThink, flavor) {
+  if (creative === "totally" && prefer === "numbers" && whatDoYouThink === "front" && flavor === "chocolate") {
+    return "you should take php";
+  } else if (creative === "totally" && prefer === "art" && whatDoYouThink === "front") {
+    return "design";
+  } else if (type === "typea" && creative === "totally" && whatDoYouThink === "back" && flavor === "vanilla") {
+    return "ruby";
+  } else if (type === "typea" && creative === "sorta" && prefer === "numbers" && whatDoYouThink === "back" && flavor === "chocolate") {
+    return "c sharp";
   }
 };
 
 
+
 $(document).ready(function() {
-  $("#workPlease form").submit(function(event) {
+  $("#work form").submit(function(event) {
+
     var type = $("#typeab").val();
     var creative = $("#creative").val();
     var prefer = $("#prefer").val();
     var whatDoYouThink = $("#whatdoyouthink").val();
     var flavor = $("#flavor").val();
 
-    var whichToReveal = decidingForYou(type, creative, prefer, whatDoYouThink, flavor);
-
-    if (whichToReveal === results1) {
-      $("#results1").show();
-    } else if (whichToReveal === results2) {
-      $("#results2").show();
-    } else if (whichToReveal === results3) {
-      $("#results3").show();
-    } else if (whichToReveal === results4) {
-      $("#results4").show();
-    }
-    alert("hey");
+    var whichToReveal = deciding(type, creative, prefer, whatDoYouThink, flavor);
+    $(".answer").text(whichToReveal);
 
     event.preventDefault();
 

0a5510cc17cd4c9eb2e4a01f2114e3f7262baf88
Austin Blain <austin.a.blain@gmail.com>
2016-10-21 08:22:43 -0700

    add HTML CSS Bootstrap JS and JQuery but doesnt function properly

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+var decidingForYou = function(type, creative, prefer, whatDoYouThink, flavor) {
+  if (type === "Type B" && creative === "Totally" && prefer === "Art" && whatDoYouThink === "Front End for sure" && flavor === "chocolate") {
+    return results1;
+  } else if (type === "Type A" && creative === "Totally" && prefer === "Numbers" && whatDoYouThink === "Front End for sure" && flavor === "chocolate") {
+    return results4;
+  } else if (type === "Type A" && creative === "Totally" && prefer === "Numbers" && whatDoYouThink === "Back End is where the real challange is at" && flavor === "vanilla") {
+    return  results3;
+  } else if (type === "Type B" && creative === "Sort of" && prefer === "Numbers" && whatDoYouThink === "Back End is where the real challange is it" && flavor === "chocolate") {
+    return results2;
+  }
+};
+
+
+$(document).ready(function() {
+  $("#workPlease form").submit(function(event) {
+    var type = $("#typeab").val();
+    var creative = $("#creative").val();
+    var prefer = $("#prefer").val();
+    var whatDoYouThink = $("#whatdoyouthink").val();
+    var flavor = $("#flavor").val();
+
+    var whichToReveal = decidingForYou(type, creative, prefer, whatDoYouThink, flavor);
+    var post = '"' + "#" + whichToReveal + '"';
+
+    $("#results4").show();
+    alert("hey");
+
+    event.preventDefault();
+
+  });
+});
